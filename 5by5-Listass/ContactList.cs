0ab7c5651e8fb8576182841c067a25c7dbfc21ae
cauematheus23 <cauematheus_23@hotmail.com>
2024-05-06 15:57:42 -0300

    Add project files.

---
+++ 5by5-Listass/ContactList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5by5_Listass
+{
+    internal class ContactList
+    {
+        Contact? head;
+        Contact? tail;
+
+        public ContactList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        bool IsEmpty()
+        {
+            return head == null && tail == null;
+        }
+
+        public void Add(Contact contact)
+        {
+            if (IsEmpty())
+            {
+                this.head = this.tail = contact;
+            }
+            else
+            {
+                int compare = string.Compare(contact.getName(), head.getName(), comparisonType: StringComparison.OrdinalIgnoreCase);
+                if (compare <= 0)
+                {
+                    Contact aux = head;
+                    head = contact;
+                    head.setNext(aux);
+                }
+                else
+                {
+                    Contact aux = head;
+                    Contact prev = head;
+                    do
+                    {
+                        compare = string.Compare(contact.getName(), aux.getName());
+                        if (compare > 0)
+                        {
+                            prev = aux;
+                            aux = aux.getNext();
+                        }
+                        //else
+                        //{
+                        //    prev.setNext(contact);
+                        //    contact.setNext(aux);
+                            
+                        //}
+                    } while (aux != null && compare > 0);
+                    prev.setNext(contact);
+                    contact.setNext(aux);
+                    if(aux == null)
+                    {
+                        tail = contact;
+                    }
+                }
+            }
+        }
+
+        public void RemoveByName(string name)
+        {
+            if(!IsEmpty())
+            {
+                if (name == head.getName())
+                {
+                    if(head == tail)
+                    {
+                        head = tail = null;
+                    }
+                    else
+                    {
+                    head = head.getNext();
+                    }
+                }
+                else
+                {
+                    Contact aux = head;
+                    Contact prev = head;
+                    bool compare;
+                    do
+                    {
+                        compare = name.Equals(aux.getName());
+                        if (!compare)
+                        {
+                            prev = aux;
+                            aux = aux.getNext();
+                        }
+                        else
+                        {
+                            prev.setNext(aux.getNext());
+                            if (prev.getNext() == null)
+                            {
+                                tail = prev;
+                            }
+                        }
+
+                    } while(compare == false && aux != null);
+
+                    if(aux == null)
+                    {
+                        Console.WriteLine("Não existe o contato na lista");
+                    }
+
+          
+
+                }
+            }
+        }
+    }
+}
---
+++ 5by5-Listass/Program.cs
@@ -0,0 +1,32 @@
+﻿using _5by5_Listass;
+
+namespace _5by5_Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contact> list = new();
+            list.Add(new("Guilherme", "123"));
+            foreach (Contact item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            
+
+
+
+
+
+            //ContactList l1 = new ContactList();
+
+            //Contact contact = new("Bernardo", "123");
+            //l1.Add(contact);
+            //Contact contact1 = new("Ana", "456");
+            //l1.Add(contact1);
+            //Contact contact2 = new("Caue", "789");
+            //l1.Add(contact2);
+            //l1.RemoveByName("Bernardo");
+        }
+    }
+}
